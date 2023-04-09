fae7bfde82e7022c53d9fbb4fbb04a8620d6bd8d
mdaminmilton6277 <mdamin6277@gmail.com>
2023-04-09 14:04:35 -0500

    reload

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Main from "./components/main";
+import Order from "./components/Order/Order";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+        loader: () => fetch("tshirt.json"),
+      },
+      {
+        path: "/order",
+        element: <Order></Order>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
